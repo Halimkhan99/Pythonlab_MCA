c5eaf96b269aa84c15725e57fc82b669a1168bd4
Halimkhan99 <77796615+Halimkhan99@users.noreply.github.com>
2021-01-22 10:46:27 +0530

    Add files via upload

--- Q1.py
+++ Q1.py
@@ -1,1 +1,10 @@
-print("abdul halim khan")
+#Read marks of two subjects
+print("Enter marks of two subjects: ")
+S1=int(input())
+S2=int(input())
+#Calculate total, average one by one
+total = S1 + S2;
+average = total/2;
+#Print the result
+print("Total marks = ", total)
+print("Average marks = ", average)
